a58c862739fbcde91c8dc2819f52020b010e3511
le quang hieu <lequanghieu2812.dhk19@st.haui.edu.vn>
2026-09-22 14:28:41 +0700

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/ttcsn/movie_ticket_backend/MovieTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ttcsn.movie_ticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttcsn/movie_ticket_backend/MovieTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcsn.movie_ticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
